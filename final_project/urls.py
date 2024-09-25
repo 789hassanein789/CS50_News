445edd55d5b6cf1c3ac6c0129c901b5c91dd53d4
hassanein <hassanein582@gmail.com>
2024-09-25 11:43:56 +0300

    the social authentication nightmare

--- final_project/urls.py
+++ final_project/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin', admin.site.urls),
+    path('accounts/', include('allauth.urls') ),
     path('', include("CS50_News.urls"))
 ]
 
